98dd474f74c62a11b50a634fe41828d9b7b3d010
phamdangtrung <55026963+dnhoangson@users.noreply.github.com>
2020-06-30 15:03:04 +0700

    Added swagger
    Added swagger for api version control and testing

---
+++ DataManagerAPI/App_Start/AuthTokenOperation.cs
@@ -0,0 +1,51 @@
+﻿using Swashbuckle.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Web.Http.Description;
+
+namespace DataManagerAPI.App_Start
+{
+    public class AuthTokenOperation : IDocumentFilter
+    {
+        public void Apply(SwaggerDocument swaggerDoc, SchemaRegistry schemaRegistry, IApiExplorer apiExplorer)
+        {
+            swaggerDoc.paths.Add("/token", new PathItem
+            {
+                post = new Operation
+                {
+                    tags = new List<String> { "Auth" },
+                    consumes = new List<String>
+                    {
+                        "application/x-www-form-urlencoded"
+                    },
+                    parameters = new List<Parameter>
+                    {
+                        new Parameter
+                        {
+                            type = "string",
+                            name = "grant_type",
+                            required = true,
+                            @in = "formData",
+                            @default = "password"
+                        },
+                        new Parameter
+                        {
+                            type = "string",
+                            name = "username",
+                            required = false,
+                            @in = "formData"
+                        },
+                        new Parameter
+                        {
+                            type = "string",
+                            name = "password",
+                            required = false,
+                            @in = "formData"
+                        }
+                    }
+                }
+
+            });
+        }
+    }
+}
