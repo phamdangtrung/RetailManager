4bbf2472efb3f292dc6d9576a57ee9aaef30d72e
Đỗ Nguyễn Hoàng Sơn <dnhoangson.danang@outlook.com>
2020-08-15 15:42:35 +0700

    Error Handling for Login Form
    Added "ErrorMessage" for Login Form.
    "ErrorMessage" will appear and notify when user is logging in with wrong input.

--- RMDesktopUI/ViewModels/LoginViewModel.cs
+++ RMDesktopUI/ViewModels/LoginViewModel.cs
@@ -13,6 +13,7 @@ namespace RMDesktopUI.ViewModels
     {
         private string _userName;
         private string _password;
+        private string _errorMessage;
         private IAPIHelper _apiHelper;
         public LoginViewModel(IAPIHelper apiHelper)
         {
@@ -41,6 +42,34 @@ namespace RMDesktopUI.ViewModels
             }
         }
 
+        
+
+        public bool IsErrorVisible
+        {
+            get 
+            {
+                bool output = false;
+                if(ErrorMessage?.Length > 0)
+                {
+                    output = true;
+                }
+                return output; 
+            }
+        }
+
+        
+
+        public string ErrorMessage
+        {
+            get { return _errorMessage; }
+            set 
+            { 
+                _errorMessage = value;
+                NotifyOfPropertyChange(() => ErrorMessage);
+                NotifyOfPropertyChange(() => IsErrorVisible);
+            }
+        }
+
         public bool CanLogin
         {
             get
@@ -58,12 +87,12 @@ namespace RMDesktopUI.ViewModels
         {
             try
             {
+                ErrorMessage = "";
                 var result = await _apiHelper.Authenticate(UserName, Password);
             }
             catch (Exception ex)
             {
-
-                Console.WriteLine(ex.Message);
+                ErrorMessage = ex.Message;                
             }
         }
     }
