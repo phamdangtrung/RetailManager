78d5710343c0e4f88459a8210e07ad40dc6bb045
dnhoangson <55026963+dnhoangson@users.noreply.github.com>
2020-12-06 23:51:30 +0700

    Added ForeignKey constraints

--- RMDatabase/dbo/Tables/SaleDetails.sql
+++ RMDatabase/dbo/Tables/SaleDetails.sql
@@ -5,5 +5,7 @@
     [ProductId] INT NOT NULL, 
     [Quantity] MONEY NOT NULL DEFAULT 1,
     [PurchasePrice] MONEY NOT NULL, 
-    [Tax] MONEY NOT NULL DEFAULT 0
+    [Tax] MONEY NOT NULL DEFAULT 0, 
+    CONSTRAINT [FK_SaleDetails_ToSales] FOREIGN KEY (SaleId) REFERENCES Sales(Id), 
+    CONSTRAINT [FK_SaleDetails_ToProducts] FOREIGN KEY (ProductId) REFERENCES Products(Id)
 )
